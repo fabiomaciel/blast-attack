6c1f0750e637f3a67aada3846bf32f815809205c
Johnny Richard <johnny@johnnyrichard.com>
2021-10-28 02:59:05 +0200

    docs: Add BSD 3-Clause License
    Signed-off-by: Johnny Richard <johnny@johnnyrichard.com>

--- src/main.c
+++ src/main.c
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2021, Johnny Richard
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -8,7 +38,7 @@
 const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 
-int 
+int
 main (int   argc,
       char *args[])
 {
@@ -21,10 +51,10 @@ main (int   argc,
   }
 
   window = SDL_CreateWindow(
-      "SDL Tutorial", 
-      SDL_WINDOWPOS_UNDEFINED, 
-      SDL_WINDOWPOS_UNDEFINED, 
-      SCREEN_WIDTH, SCREEN_HEIGHT, 
+      "SDL Tutorial",
+      SDL_WINDOWPOS_UNDEFINED,
+      SDL_WINDOWPOS_UNDEFINED,
+      SCREEN_WIDTH, SCREEN_HEIGHT,
       SDL_WINDOW_SHOWN
   );
 
@@ -32,7 +62,7 @@ main (int   argc,
     fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
     return EXIT_FAILURE;
   }
-  
+
   SDL_Rect rect = { .x = 0, .y = 0, .w = 20, .h = 20 };
 
   bool quit = false;
@@ -43,7 +73,7 @@ main (int   argc,
     SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 
     SDL_Event event;
-    while (SDL_PollEvent(&event) != 0) { 
+    while (SDL_PollEvent(&event) != 0) {
       if (event.type == SDL_QUIT) {
         quit = true;
       }
